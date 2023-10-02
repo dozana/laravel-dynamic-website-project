f91cb238ce53b9792a5b07c846c91aa83f1df123
Levan <levan.soroznishvili@gmail.com>
2023-10-02 22:35:40 +0400

    Temporary DemoController

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Demo\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'index')->name('about.page');
+    Route::get('/contact', 'contact')->name('contact.page');
 });
