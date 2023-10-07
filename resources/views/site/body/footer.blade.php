bee896cc8c655aad289b267b3843957d4d44849e
Levan <levan.soroznishvili@gmail.com>
2023-10-07 19:59:41 +0400

    Frontend layout segmentation

---
+++ resources/views/site/body/footer.blade.php
@@ -0,0 +1,56 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-lg-4">
+                <div class="footer__widget">
+                    <div class="fw-title">
+                        <h5 class="sub-title">Contact us</h5>
+                        <h4 class="title">+995 123 456</h4>
+                    </div>
+                    <div class="footer__widget__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae commodi consequatur deserunt doloremque eum id impedit inventore libero nam omnis, quae quas quasi soluta temporibus tenetur, velit! Corporis dolor obcaecati rem rerum, vitae voluptatibus.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-sm-6">
+                <div class="footer__widget">
+                    <div class="fw-title">
+                        <h5 class="sub-title">my address</h5>
+                        <h4 class="title">GEORGIA</h4>
+                    </div>
+                    <div class="footer__widget__address">
+                        <p>Tsereteli St. #0, Tbilisi <br> Georgia </p>
+                        <a href="mailto:noreply@dynamic.com" class="mail">noreply@dynamic.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-sm-6">
+                <div class="footer__widget">
+                    <div class="fw-title">
+                        <h5 class="sub-title">Follow me</h5>
+                        <h4 class="title">socially connect</h4>
+                    </div>
+                    <div class="footer__widget__social">
+                        <p>Lorem ipsum dolor sit amet enim. <br> Etiam ullamcorper.</p>
+                        <ul class="footer__social__list">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright__wrap">
+            <div class="row">
+                <div class="col-12">
+                    <div class="copyright__text text-center">
+                        <p>Copyright @ Dynamic Website 2023 All right Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
